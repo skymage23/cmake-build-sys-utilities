8cfc1c8a5202dcc452440774f487ec79b1a5b8d4
Christopher Loyd Nugent <skymage23.cn@gmail.com>
2025-02-21 15:23:21 -0600

    Re-enabled all previously disabled test in "tests/run_tests.cmake".

--- tests/run_tests.cmake
+++ tests/run_tests.cmake
@@ -8,4 +8,5 @@ include("${CMAKE_CURRENT_LIST_DIR}/third_party/cmake-script-test-framework/cmake
 run_test(TEST_SCRIPT_FILE "${CMAKE_CURRENT_LIST_DIR}/test_get_dirs_with_cmake_lists.cmake")
 run_test(TEST_SCRIPT_FILE "${CMAKE_CURRENT_LIST_DIR}/test_get_targets_from_project_tree.cmake")
 run_test(TEST_SCRIPT_FILE "${CMAKE_CURRENT_LIST_DIR}/test_run_cmake_script.cmake")
+run_test(TEST_SCRIPT_FILE "${CMAKE_CURRENT_LIST_DIR}/test_look_for_dependency_program.cmake")
 
