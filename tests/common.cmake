8772bb4bd6a3e50fa151c2a2ed7be4512fdacc0d
Christopher Loyd Nugent <skymage23.cn@gmail.com>
2025-02-22 19:39:09 -0800

    Moved some of the testing files out of the test_helpers directory because the Windows SDK tools have odd quirks stemming from the before-times of pre-1999 such as a maximum filepath lengths that have to be abided by else the build fail with a "mysterious" and non-specific error message. Anyway, the tests now work properly. If anyone from Microsoft sees these, please put in a word for me to see if these bugs can be fixed.

--- tests/common.cmake
+++ tests/common.cmake
@@ -1,4 +1,4 @@
 include_guard(GLOBAL)
 include("${CMAKE_CURRENT_LIST_DIR}/../scripts/common.cmake")
 
-file(REAL_PATH "${SCRIPTS_DIR}/../tests/test_helpers" TEST_HELPERS_DIR)
+file(REAL_PATH "${SCRIPTS_DIR}/../tests" TEST_HELPERS_DIR)
