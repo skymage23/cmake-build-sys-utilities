d862eeb8e992efcf158fb5ca050acd58c7ea007f
Christopher Loyd Nugent <skymage23.cn@gmail.com>
2025-05-01 16:22:46 -0500

    In "scripts/common.cmake", switched the call to "file" to get the absolute path of the "tests" directory to a regular "set" call as it is no longer necessary to attempt to resolve the absolute path here.
    Added TESTS_DIR, TEST_SCRIPTS_DIR, TESTS_THIRD_PARTY_DIR, and CMAKE_TESTS_FRAMEWORK_DIR and set their values accordingly.

--- tests/common.cmake
+++ tests/common.cmake
@@ -1,4 +1,7 @@
 include_guard(GLOBAL)
 include("${CMAKE_CURRENT_LIST_DIR}/../scripts/common.cmake")
 
-file(REAL_PATH "${SCRIPTS_DIR}/../tests" TEST_HELPERS_DIR)
+set(TESTS_DIR "${MODULE_BASE}/tests")
+set(TESTS_SCRIPTS_DIR "${TESTS_DIR}/scripts")
+set(TESTS_THIRD_PARTY_DIR "${TESTS_DIR}/third_party")
+set(CMAKE_TESTS_FRAMEWORK_DIR "${TESTS_THIRD_PARTY_DIR}/cmake-script-test-framework")
