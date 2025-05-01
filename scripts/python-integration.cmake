a60f36fb61c80dedf659419a28f293c89a40195e
Christopher Loyd Nugent <skymage23.cn@gmail.com>
2025-05-01 16:17:47 -0500

    Added "python-integration.cmake" to make invoking Python from CMake a little easier.

---
+++ scripts/python-integration.cmake
@@ -0,0 +1,7 @@
+include_guard(GLOBAL)
+find_package(Python3)
+set(PYTHON_SCRIPT_DIR_PATH "${CMAKE_CURRENT_LIST_DIR}/python")
+set(PYTHON_SCRIPT_WORKING_DIR "${CMAKE_CURRENT_LIST_DIR}/..")
+
+set(cmd_list_template "")
+list(APPEND python_cmd_list_template "${Python3_EXECUTABLE}")
